21fb201e04bedfb1a5dc07c28715beaa83b20605
IT23767140 <it23767140@my.sliit.lk>
2025-05-22 13:36:09 +0530

    init commit

---
+++ src/main/java/com/PGN24/tutorbooking/TutorbookingApplication.java
@@ -0,0 +1,13 @@
+package com.PGN24.tutorbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorbookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorbookingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/PGN24/tutorbooking/TutorbookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PGN24.tutorbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorbookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
